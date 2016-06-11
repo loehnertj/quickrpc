1abc390108dd264cc71d50b284efd9253e83fc25
Johannes Loehnert <loehnert.kde@gmx.net>
2016-06-11 22:58:42 +0200

    actually render outlines of pieces!

--- cluster_widget.py
+++ cluster_widget.py
@@ -3,8 +3,22 @@ import logging as L
 import os
 
 from PyQt4.QtCore import Qt, QPointF, QSizeF, QSize, QRectF
-from PyQt4.QtGui import QPixmap, QColor, QStaticText, QStyle
+from PyQt4.QtGui import QImage, QPixmap, QColor, QStaticText, QStyle
 from PyQt4.QtGui import QGraphicsItem, QGraphicsWidget, QGraphicsPixmapItem
+
+from render_outline import outline
+
+class PieceItem(QGraphicsPixmapItem):
+    def __init__(o, path, parent):
+        QGraphicsPixmapItem.__init__(o, parent=parent)
+        o.img = QImage()
+        o.img.load(path)
+        o.updateRotation(0)
+        
+    def updateRotation(o, angle_deg):
+        img = o.img.copy(o.img.rect())
+        outline(img, illum_angle=-angle_deg-30)
+        o.setPixmap(QPixmap.fromImage(img))
 
 class ClusterWidget(QGraphicsWidget):
     def __init__(o, puzzle_board, cluster):
@@ -15,11 +29,9 @@ class ClusterWidget(QGraphicsWidget):
         for piece in cluster.pieces:
             o.add_piece(piece)
             
-    def add_piece(o, piece, pixmap=None):
-        if not pixmap:
-            path = os.path.join(o.puzzle_board.imagefolder, piece.image)
-            pixmap = QPixmap(path)
-        item = QGraphicsPixmapItem(pixmap, parent=o)
+    def add_piece(o, piece):
+        path = os.path.join(o.puzzle_board.imagefolder, piece.image)
+        item = PieceItem(path, parent=o)
         item.setPos(piece.x0, piece.y0)
             
     def boundingRect(o):
@@ -33,3 +45,8 @@ class ClusterWidget(QGraphicsWidget):
     def updatePos(o):
         o.setPos(o.cluster.x, o.cluster.y)
         o.setRotation(-360.*o.cluster.rotation/o.cluster.rotations)
+
+    def setRotation(o, angle_deg):
+        QGraphicsWidget.setRotation(o, angle_deg)
+        for item in o.childItems():
+            item.updateRotation(angle_deg)
