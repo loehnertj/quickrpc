33f5777bad9f206703fe997f51a4c7d628123563
Johannes Loehnert <loehnert.kde@gmx.net>
2016-06-30 21:18:19 +0200

    add stop method and treat leftovers properly

--- neatocom/stdio_transport.py
+++ neatocom/stdio_transport.py
@@ -2,22 +2,25 @@ import sys
 from .concepts import Transport
 
 class StdioTransport(Transport):
+    def stop(self):
+        self.running=False
+    
     def send(self, data, receivers=None):
         sys.stdout.buffer.write(data)
         
     def run(self):
         '''run, blocking.'''
-        while True:
+        self.running = True
+        leftover = b''
+        while self.running:
             # FIXME: read binary
             data = input().encode('utf8')
-            if data==b'':
-                return
-            leftover = self.received(sender='stdio', data=data)
+            leftover = self.received(sender='stdio', data=leftover+data)
             
 
 def test():
     t = StdioTransport()
-    print('enter something, empty line to quit')
+    print('enter something, Ctrl+C to quit')
     t.run()
     
 if __name__=='__main__':
