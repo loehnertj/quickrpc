90441fda57229419f2a4c3f19d3730fa1e071893
Johannes Loehnert <loehnert.kde@gmx.net>
2016-07-07 21:26:36 +0200

    added quit() message to EchoAPI

--- neatocom/echo_api.py
+++ neatocom/echo_api.py
@@ -1,30 +1,34 @@
 from .concepts import RemoteAPI, incoming, outgoing
 
+
 class EchoAPI(RemoteAPI):
     '''Demo of how to use RemoteAPI.
-    
+
     Echo API answers incoming `say` calls with an `echo` call.
     '''
     @incoming
     def say(self, sender="", text=""): pass
-        
+
     @outgoing
     def echo(self, receivers=None, text=""): pass
 
+    @incoming
+    def quit(self, sender=""): pass
 
 
 def test():
     from .json_codec import JsonCodec
     from .stdio_transport import StdioTransport
-    
+
     t = StdioTransport()
     api = EchoAPI(codec=JsonCodec(), transport=t)
     api.say.connect(lambda sender="", text="": api.echo(text=text))
-    
-    print('supported: {"__method":"say", "text": "hello world"}')
+    api.quit.connect(lambda sender="": t.stop())
+
+    # print('supported: {"__method":"say", "text": "hello world"} and {"__method":"quit"}')
     t.run()
-    
-if __name__=='__main__':
+
+if __name__ == '__main__':
     import logging
-    logging.basicConfig(level='DEBUG')
+    logging.basicConfig(level='DEBUG', filename='echo_api.log')
     test()
