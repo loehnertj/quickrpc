287987221161df3a14a30f99126f2085bad1329d
Johannes Loehnert <loehnert.kde@gmx.net>
2016-11-23 21:16:02 +0100

    echo api: use/demo MuxTransport+StdioTransport

--- neatocom/echo_api.py
+++ neatocom/echo_api.py
@@ -19,16 +19,18 @@ class EchoAPI(RemoteAPI):
 def test():
     from .json_codec import JsonCodec
     from .stdio_transport import StdioTransport
+    from .mux_transport import MuxTransport
 
     t = StdioTransport()
-    api = EchoAPI(codec=JsonCodec(), transport=t)
+    mt = MuxTransport()
+    mt += t
+    api = EchoAPI(codec=JsonCodec(), transport=mt)
     api.say.connect(lambda sender="", text="": api.echo(text=text))
-    api.quit.connect(lambda sender="": t.stop())
-
-    # print('supported: {"__method":"say", "text": "hello world"} and {"__method":"quit"}')
-    t.run()
+    api.quit.connect(lambda sender="": mt.stop())
+    mt.run()
 
 if __name__ == '__main__':
     import logging
+    print('supported: {"__method":"say", "text": "hello world"} and {"__method":"quit"}')
     logging.basicConfig(level='DEBUG', filename='echo_api.log')
     test()
