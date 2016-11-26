de9c0b18a46ac3159034556f0a47d2eddc8d1013
Johannes Loehnert <loehnert.kde@gmx.net>
2016-11-26 18:25:32 +0100

    make echo_api.test() into a full chat server :-)

--- neatocom/echo_api.py
+++ neatocom/echo_api.py
@@ -1,3 +1,18 @@
+'''EchoAPI: simple chat server.
+
+Demonstrates use of RemoteAPI as well as StdioTransport and TcpServerTransport.
+
+The server responds to a "say" call with "echo" of the text to all clients.
+The message "quit":
+ - if coming from stdio, shuts the server down
+ - if coming over a tcp connection, makes the server close the connection.
+
+Run with python3 -m neatocom.echo_api.
+Enter json messages on the commandline to test stdio transport.
+Use `telnet localhost 8888` to test tcp functionality.
+Use `tail -F echo_api.log` in another terminal to watch logged events.
+'''
+
 from .concepts import RemoteAPI, incoming, outgoing
 
 
@@ -16,18 +31,35 @@ class EchoAPI(RemoteAPI):
     def quit(self, sender=""): pass
 
 
+
 def test():
     from .json_codec import JsonCodec
     from .stdio_transport import StdioTransport
     from .mux_transport import MuxTransport
+    from .tcp_server_transport import TcpServerTransport
+    
+    L().info('Start echo_api.test')
 
-    t = StdioTransport()
     mt = MuxTransport()
-    mt += t
+    mt += StdioTransport()
+    server = TcpServerTransport(port=8888) 
+    mt += server
+    print('serving on port 8888')
     api = EchoAPI(codec=JsonCodec(), transport=mt)
     api.say.connect(lambda sender="", text="": api.echo(text=text))
-    api.quit.connect(lambda sender="": mt.stop())
+    
+    def onquit(sender):
+        if sender=='stdio':
+            print('Server stops.')
+            mt.stop()
+        else:
+            print('Disconnect %s.'%sender)
+            server.close(sender)
+    api.quit.connect(onquit)
+    
     mt.run()
+    
+    L().info('Exit echo_api.test')
 
 if __name__ == '__main__':
     import logging
