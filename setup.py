92e3a285304db87431c2c9b8b946541fb5609f02
Johannes Loehnert <loehnert.kde@gmx.net>
2017-12-20 21:45:03 +0100

    add setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+# better name: humblerpc?
+
+setup(name='neatocom',
+      version='0.1',
+      description='Small, interoperable, automagic RPC library.',
+      url='http://github.com/loehnertj/neatocom',
+      author='Johannes Loehnert',
+      author_email='loehnert.kde@gmx.de',
+      license='LGPL',
+      packages=['neatocom'],
+      zip_safe=True)
