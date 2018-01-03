e9a6cd2a354b75ab5d3683f6928937dd94358a43
Johannes Loehnert <loehnert.kde@gmx.net>
2018-01-03 21:42:46 +0100

    MIT licensed, add classifiers

--- setup.py
+++ setup.py
@@ -1,13 +1,17 @@
 from setuptools import setup
 
-# better name: humblerpc?
-
 setup(name='quickrpc',
       version='0.1',
       description='Small, interoperable, automagic RPC library.',
       url='http://github.com/loehnertj/quickrpc',
       author='Johannes Loehnert',
       author_email='loehnert.kde@gmx.de',
-      license='LGPL',
+      license='MIT',
+      classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: System :: Networking',
+      ],
       packages=['quickrpc'],
       zip_safe=True)
