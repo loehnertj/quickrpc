1b702ab3734a5dd4e4ace9a45c17f9ac92cdc629
Johannes Loehnert <loehnert.kde@gmx.net>
2018-01-04 21:26:43 +0100

    expose the most-needed members on package toplevel

--- quickrpc/__init__.py
+++ quickrpc/__init__.py
@@ -4,3 +4,22 @@
 * Codec: decodes/encodes messages into bytes.
 * RemoteAPI: a class whose methods correspond to outgoing / incoming remote calls.
 '''
+
+from . import transports
+# import, so that subclasses become known
+from . import network_transports
+from . import codecs
+from . import terse_codec
+from .remote_api import RemoteAPI, incoming, outgoing
+
+__all__ = [
+        'RemoteAPI',
+        'incoming',
+        'outgoing',
+        'transport',
+        'codec'
+        ]
+
+
+transport = transports.Transport.fromstring
+codec = codecs.Codec.fromstring
