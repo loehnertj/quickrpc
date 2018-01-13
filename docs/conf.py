016e812621b989f4f943529b45f900974214b5ac
Johannes Loehnert <loehnert.kde@gmx.net>
2018-01-13 21:01:37 +0100

    readthedocs, next try

--- docs/conf.py
+++ docs/conf.py
@@ -24,7 +24,7 @@ class Mock(MagicMock):
     def __getattr__(cls, name):
             return MagicMock()
 
-MOCK_MODULES = ['PyQt4',]
+MOCK_MODULES = ['PyQt4', 'QtCore', 'QtNetwork']
 sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
 
